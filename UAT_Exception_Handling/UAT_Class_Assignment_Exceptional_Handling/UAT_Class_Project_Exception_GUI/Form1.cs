e70b6b9c5c12f3f1af63ba344c95db1452a47405
Clarence Brathwaite <Clarence.Brathwaite@ey.com>
2020-10-09 09:19:45 -0400

    GUI for class assignment

---
+++ UAT_Exception_Handling/UAT_Class_Assignment_Exceptional_Handling/UAT_Class_Project_Exception_GUI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UAT_Class_Project_Exception_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var userInput = textBox1.Text;
+            int data = 0;
+            Int32.TryParse(userInput, out data);
+   
+            // Initializes the variables to pass to the MessageBox.Show method.
+            string message = "You did not enter an integer. Cancel this operation?";
+            string caption = "Error Detected in Input";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            if (data > 0)
+            {
+                message = "Good valid user input. The process will continue.";
+                caption = "";
+                MessageBox.Show(message, caption);
+            }
+            if (data == 0)
+            {
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    // Closes the parent form.
+                    this.Close();
+                }
+            }
+        }
+
+    }
+}
+
